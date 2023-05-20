70f7ce6c73442ee6580e872c7e6a95429e108ac7
hong <kevin0928@naver.com>
2023-05-20 19:41:12 +0900

    [FEAT] add server heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/build/libs/restful_sign_project-0.0.1-SNAPSHOT.jar
--- src/main/java/restful_sign_project/RestfulSignProjectApplication.java
+++ src/main/java/restful_sign_project/RestfulSignProjectApplication.java
@@ -2,6 +2,9 @@ package restful_sign_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulSignProjectApplication {
@@ -9,5 +12,15 @@ public class RestfulSignProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulSignProjectApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 
 }
+

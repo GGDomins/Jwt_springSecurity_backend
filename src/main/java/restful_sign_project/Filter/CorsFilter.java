09076c29b01474e992e178364e0a4c1f3dfe5597
hong <kevin0928@naver.com>
2023-05-21 16:36:15 +0900

    [FIX] allowOrigins

--- src/main/java/restful_sign_project/Filter/CorsFilter.java
+++ src/main/java/restful_sign_project/Filter/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        response.setHeader("Access-Control-Allow-Origin", "https://restful-jwt-project.herokuapp.com/");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
